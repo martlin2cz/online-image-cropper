ca5971265716913ea3407b428fbcce84ec6b107a
m@rtlin2cz <martlin2cz@gmail.com>
2017-07-28 19:57:19 +0200

    Somehow working version (before cleanup)
    	- completed cropping and corner rounding, need some clenaup and refactor
    	- UI needs update

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,21 @@
 
 var oic = {};
 
+var SVG_SIZE = 100;
 ///////////////////////////////////////////////////////////////////////////////
+oic.begin = function() {
+	this.changeZoom();
+
+	var img = this.inferFromUrl('img');
+	//console.log("image: " + img);
+
+	if (img) {
+		var imgInput = document.getElementById('input-url');
+		imgInput.value = img;
+		this.loadInputImage();
+	}
+}
+
 
 oic.changeZoom = function() {
 	var zoomInput = document.getElementById('input-zoom');
@@ -32,25 +46,100 @@ oic.loadInputImage =  function() {
 ///////////////////////////////////////////////////////////////////////////////
 
 oic.update = function() {
+	this.performCrop();
+	this.performRound();
+	this.setupImage();
+	return this.generateOutput();
+	
+}
+
+oic.setupImage = function() {
 	var imgInput = document.getElementById('input-image');
 	var imgOutput = document.getElementById('output-image');
 
-	var inputCropTop = document.getElementById('input-crop-top');
-	var inputRoundCorners = document.getElementById('input-round-corners');
-
-	var cropTop = inputCropTop.value;
-	var roundCorners = inputRoundCorners.value;
+	var cropped = document.getElementById('image-to-crop');	
 
 	var imageSrc = imgInput.src;
-	var newImageSrc = this.cropImage(imageSrc, cropTop, roundCorners);
-
-	imgOutput.src = newImageSrc;
+	cropped.setAttribute('xlink:href', imageSrc);
 }
 
+oic.generateOutput = function() {
+	//TODO
+	return "im://ag.e";
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 
-oic.cropImage = function(imageSrc, cropTop, roundCorners) {
-	//TODO
-	return imageSrc;
+
+
+oic.performCrop = function() {
+	var inputCropTop = document.getElementById('input-crop-top');
+	var inputCropBottom = document.getElementById('input-crop-bottom');
+	var inputCropLeft = document.getElementById('input-crop-left');
+	var inputCropRight = document.getElementById('input-crop-right');
+	
+
+	var cropTop = parseInt(inputCropTop.value);
+	var cropBottom = parseInt(inputCropBottom.value);
+	var cropLeft = parseInt(inputCropLeft.value);
+	var cropRight = parseInt(inputCropRight.value);
+
+	if (cropTop >= cropBottom) {
+		console.warn("T-B: " + cropTop + " - " + cropBottom);
+		return;
+	}
+
+	if (cropLeft >= cropRight) {
+		console.warn("L-R: " + cropLeft + " - " + cropRight);
+		return;
+	}
+
+	this.setCrop(cropTop, cropBottom, cropLeft, cropRight);
+}
+
+oic.performRound = function() {
+	var inputRoundCorners = document.getElementById('input-round-corners');
+	
+	var roundCorners = parseInt(inputRoundCorners.value) / 2;
+
+	this.setRound(roundCorners);
+}
+///////////////////////////////////////////////////////////////////////////////
+
+oic.setCrop = function(cropTop, cropBottom, cropLeft, cropRight) {
+	console.log('Cropping  t' + cropTop + ",  b" + cropBottom + ",  l" + cropLeft + ",  r" + cropRight);
+	var cropper = document.getElementById('total-crop-rect');
+
+	cropper.setAttribute('y', cropTop);
+	cropper.setAttribute('height', cropBottom - cropTop);
+	cropper.setAttribute('x', cropLeft);
+	cropper.setAttribute('width', cropRight - cropLeft);
+}
+
+oic.setRound = function(roundCorners) {
+	console.log('Rounding r' + roundCorners);
+
+	var cropper = document.getElementById('total-crop-rect');
+
+	cropper.setAttribute('rx', roundCorners);
+	cropper.setAttribute('ry', roundCorners);
+}
+	
+///////////////////////////////////////////////////////////////////////////////
+
+oic.inferFromUrl = function(key) {
+	var query = location.search;
+	var tuples = query.split(/[&?]/);
+	//console.log(tuples);
+
+	for (var i = 0; i < tuples.length; i++) {
+		var tuple = tuples[i];
+		var parts = tuple.split('=');
+
+		if (parts[0] == key) {
+			return parts[1];
+		}
+	}
+
+	return null;
 }
